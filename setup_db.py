267b2ee432a4c9f6c1abe32123c848dc718a831a
Chris Hallberg <crhallberg@gmail.com>
2018-04-04 16:03:32 -0400

    Add counts. Add exclusions.

--- setup_db.py
+++ setup_db.py
@@ -32,7 +32,7 @@ conn.commit()
 print ("Create librarians...")
 cursor.execute("""CREATE TABLE librarians
                     (id integer primary key autoincrement, name varchar unique, initials varchar unique);""")
-librarians = json.load(open("setup_data/librarians.json", "r")) # librarian data by initial
+librarians = json.load(open("db_data/librarians.json", "r")) # librarian data by initial
 for initials in librarians:
     cursor.execute("INSERT INTO librarians(initials, name) VALUES (?,?);", (initials, librarians[initials]["name"]))
     print ("\t", librarians[initials]["name"], cursor.lastrowid)
@@ -41,40 +41,38 @@ conn.commit()
 
 print ("Create callnumbers...")
 cursor.execute("""CREATE TABLE callnumbers
-                    (section varchar primary key, collection_total integer, gg_recommended integer, category integer, assigned_to integer);""")
-callnumbers = json.load(open("setup_data/callnumbers.json", "r")) # callnumbers data by section
+                    (section varchar primary key, assigned_to integer, subject integer,
+                     collection_total integer, gg_recommended integer, reviewed_count integer, posted_count integer);""")
+callnumbers = json.load(open("db_data/callnumbers.json", "r")) # callnumbers data by section
 callnumbers_sorted = [key for key in callnumbers]
 callnumbers_sorted.sort()
 for section in callnumbers_sorted:
-    category = -1
+    subject = -1
     assigned_to = -1
     for initials in librarians:
         if section in librarians[initials]["assignment"]:
-            category = subjects[librarians[initials]["subject"]]
+            subject = subjects[librarians[initials]["subject"]]
             assigned_to = librarians[initials]["id"]
-    if category == -1:
+    if subject == -1:
         print ("missing", section)
         break
     cursor.execute(
-        "INSERT INTO callnumbers(section, collection_total, gg_recommended, category, assigned_to) VALUES (?,?,?,?,?);",
-        (section, callnumbers[section]["collection"], callnumbers[section]["gg_total"], category, assigned_to)
+        "INSERT INTO callnumbers(section, collection_total, gg_recommended, subject, assigned_to) VALUES (?,?,?,?,?);",
+        (section, callnumbers[section]["collection"], callnumbers[section]["gg_total"], subject, assigned_to)
     )
     print ("\t", section, cursor.lastrowid, assigned_to)
 conn.commit()
 
-
-# - Create tables to be populated by other scriptscursor.execute("""CREATE TABLE faculty
-                    (id integer primary key autoincrement, name varchar unique, location varchar, department varchar, email varchar);""")
+# - Create tables to be populated by other scriptscursor.execute("CREATE TABLE excluded (barcode integer primary key, reason text);")
+cursor.execute("""CREATE TABLE faculty
+                    (id integer primary key autoincrement, name varchar unique, department varchar, address varchar, email varchar);""")
 cursor.execute("""CREATE TABLE faculty_requests
-                    (id integer primary key autoincrement, date datetime, faculty integer);""")
+                    (id integer primary key autoincrement, date timestamp, faculty integer);""")
 cursor.execute("""CREATE TABLE book_requests
-                    (barcode integer primary key, request integer, personal boolean);""")
+                    (id integer primary key autoincrement, barcode integer, cn_section varchar,
+                     request integer, personal boolean, comment text, from_file integer);""")
+cursor.execute("""CREATE TABLE posted_files
+                    (id integer primary key autoincrement, name varchar, created_by integer, item_count integer);""")
 conn.commit()
 
-cursor.execute("""SELECT SUM(callnumbers.gg_recommended) FROM callnumbers
-    INNER JOIN librarians ON librarians.id = callnumbers.assigned_to
-    WHERE librarians.name = 'Darren Poley';""")
-print (cursor.fetchall())
-
-conn.commit()
 conn.close()
