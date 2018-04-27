b1cdaefe38908427f1b3071fca9906af2796a268
Chris Hallberg <crhallberg@gmail.com>
2018-04-27 12:50:13 -0400

    Update remove_checkedout_to use database.

--- src/posted_files.py
+++ src/posted_files.py
@@ -5,49 +5,56 @@ from xlrd import open_workbook  # Excel files
 
 from .utils import normalize_callnumber
 
+def parse_source_file(filename):
+    posted_file = {
+        "name": filename
+    }
+    with open_workbook(filename) as excel_book:
+        sheet = excel_book.sheet_by_index(0)
+        rvalues = sheet.row_values(0)
+        column_names = {
+            "Display Call Number": "callnumber",
+            "Barcode": "barcode",
+            "Title": "title",
+            "Author": "author",
+            "Publication Year": "year",
+        }
+        books_columns = {}
+        for name in column_names:
+            if name in rvalues:
+                books_columns[column_names[name]] = rvalues.index(name)
+            else:
+                for i in range(len(rvalues)):
+                    print ("x\t%s:" % filename)
+                    print ("x\t%s" % (i, rvalues[i]))
+                    raise Error("Invalid columns")
+
+        path = filename.split("\\")
+        check_letter = re.search("^([A-Z]+)", path[len(path) - 1]).group(1)
+        print (check_letter)
+        books = []
+        for row in range(1, sheet.nrows):
+            rvalues = sheet.row_values(row)
+            # print (check_letter, rvalues[books_columns["callnumber"]])
+            if not rvalues[books_columns["callnumber"]].startswith(check_letter):
+                continue
+            book = {}
+            for key in books_columns:
+                book[key] = str(rvalues[books_columns[key]]).split(".")[0]
+            # Special treatment
+            book["year"] = str(rvalues[books_columns["year"]]).split(".")[0]
+            book["callnumber"] = str(rvalues[books_columns["callnumber"]]).split(".")[0]
+            book["callnumber_sort"] = normalize_callnumber(book["callnumber"])
+            books.append(book)
+    posted_file["books"] = books
+    return posted_file
+
 def parse_source_dir(month_dir):
     posted_files = []
 
     for dirpath, dirnames, filenames in os.walk(month_dir):
         for filename in filenames:
-            posted_file = {
-                "name": filename,
-                "month": month_dir
-            }
-            with open_workbook(os.path.join(dirpath, filename)) as excel_book:
-                sheet = excel_book.sheet_by_index(0)
-                rvalues = sheet.row_values(0)
-                column_names = {
-                    "Display Call Number": "callnumber",
-                    "Barcode": "barcode",
-                    "Title": "title",
-                    "Author": "author",
-                    "Publication Year": "year",
-                }
-                books_columns = {}
-                for name in column_names:
-                    if name in rvalues:
-                        books_columns[column_names[name]] = rvalues.index(name)
-                    else:
-                        for i in range(len(rvalues)):
-                            print ("x\t%s:" % filename)
-                            print ("x\t%s" % (i, rvalues[i]))
-                            raise Error("Invalid columns")
-
-                check_letter = re.search("^([A-Z]+)", filename[0]).group(1)
-                books = []
-                for row in range(1, sheet.nrows):
-                    rvalues = sheet.row_values(row)
-                    if not rvalues[books_columns["callnumber"]].startswith(check_letter):
-                        continue
-                    book = {}
-                    for key in books_columns:
-                        book[key] = str(rvalues[books_columns[key]]).split(".")[0]
-                    # Special treatment
-                    book["year"] = str(rvalues[books_columns["year"]]).split(".")[0]
-                    book["callnumber"] = str(rvalues[books_columns["callnumber"]]).split(".")[0]
-                    book["callnumber_sort"] = normalize_callnumber(book["callnumber"])
-                    books.append(book)
-            posted_file["books"] = books
-            posted_files.append(posted_file)
+            parsed_file = parse_source_file(os.path.join(dirpath, filename))
+            parsed_file["month"] = month_dir
+            posted_files.append(parsed_file)
     return posted_files
